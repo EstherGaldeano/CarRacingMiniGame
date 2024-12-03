9c171046f2626525c4954e6950d273b76a1808d0
Retse_dev <stergaldeano@gmail.com>
2024-12-03 14:18:17 +0100

    menu works and cars added

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OneCarScene(){
+        SceneManager.LoadScene("1Car");
+    }
+
+    public void TwoCarScene(){
+        SceneManager.LoadScene("2Car");
+    }
+
+    public void FourCarScene(){
+        SceneManager.LoadScene("4Car");
+    }
+}
