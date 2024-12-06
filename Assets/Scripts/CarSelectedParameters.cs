1aa25f2a685dc12a115da2da405f6cc5ec5bd950
Andrés <hidalgoandres25@gmail.com>
2024-12-06 11:56:04 +0100

    colliders/pauseMenu

--- Assets/Scripts/CarSelectedParameters.cs
+++ Assets/Scripts/CarSelectedParameters.cs
@@ -7,6 +7,8 @@ public class CarSelectedParameters : MonoBehaviour
     private GameObject wheelHubs;
     [SerializeField]
     private GameObject playerCars;
+    [SerializeField]
+    private GameObject collidersCar;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,6 +25,9 @@ public class CarSelectedParameters : MonoBehaviour
             wheelHubs.gameObject.transform.GetChild(1).gameObject.transform.localPosition = new Vector3(0.71f, 0.54f, -1.76f);
             wheelHubs.gameObject.transform.GetChild(2).gameObject.transform.localPosition = new Vector3(-0.7f, 0.54f, 1.27f);
             wheelHubs.gameObject.transform.GetChild(3).gameObject.transform.localPosition = new Vector3(-0.71f, 0.54f, -1.76f);
+
+            collidersCar.gameObject.transform.GetChild(1).gameObject.GetComponent<BoxCollider>().center = new Vector3(0, 0.84f, 0.03f);
+            collidersCar.gameObject.transform.GetChild(1).gameObject.GetComponent<BoxCollider>().size = new Vector3(1.07f, 2.68f, 1.13f);
         }
         if (carSelected == 1)
         {
@@ -30,6 +35,9 @@ public class CarSelectedParameters : MonoBehaviour
             wheelHubs.gameObject.transform.GetChild(1).gameObject.transform.localPosition = new Vector3(0.6f, 0.54f, -1.86f);
             wheelHubs.gameObject.transform.GetChild(2).gameObject.transform.localPosition = new Vector3(-0.6f, 0.54f, 1.18f);
             wheelHubs.gameObject.transform.GetChild(3).gameObject.transform.localPosition = new Vector3(-0.6f, 0.54f, -1.86f);
+
+            collidersCar.gameObject.transform.GetChild(1).gameObject.GetComponent<BoxCollider>().center = new Vector3(0, 1.06f, 0.03f);
+            collidersCar.gameObject.transform.GetChild(1).gameObject.GetComponent<BoxCollider>().size = new Vector3(1f, 3.12f, 1.19f);
         }
         if (carSelected == 2)
         {
@@ -37,6 +45,9 @@ public class CarSelectedParameters : MonoBehaviour
             wheelHubs.gameObject.transform.GetChild(1).gameObject.transform.localPosition = new Vector3(0.6f, 0.54f, -2.08f);
             wheelHubs.gameObject.transform.GetChild(2).gameObject.transform.localPosition = new Vector3(-0.6f, 0.54f, 1.15f);
             wheelHubs.gameObject.transform.GetChild(3).gameObject.transform.localPosition = new Vector3(-0.6f, 0.54f, -2.08f);
+
+            collidersCar.gameObject.transform.GetChild(1).gameObject.GetComponent<BoxCollider>().center = new Vector3(0, 2.21f, -0.06f);
+            collidersCar.gameObject.transform.GetChild(1).gameObject.GetComponent<BoxCollider>().size = new Vector3(1.07f, 5.43f, 1.37f);
         }
         if (carSelected == 3)
         {
@@ -44,6 +55,9 @@ public class CarSelectedParameters : MonoBehaviour
             wheelHubs.gameObject.transform.GetChild(1).gameObject.transform.localPosition = new Vector3(0.6f, 0.54f, -1.58f);
             wheelHubs.gameObject.transform.GetChild(2).gameObject.transform.localPosition = new Vector3(-0.6f, 0.54f, 1.45f);
             wheelHubs.gameObject.transform.GetChild(3).gameObject.transform.localPosition = new Vector3(-0.6f, 0.54f, -1.58f);
+
+            collidersCar.gameObject.transform.GetChild(1).gameObject.GetComponent<BoxCollider>().center = new Vector3(0, 2.21f, 0.02f);
+            collidersCar.gameObject.transform.GetChild(1).gameObject.GetComponent<BoxCollider>().size = new Vector3(1.07f, 5.43f, 1.21f);
         }
         if (carSelected == 4)
         {
@@ -51,6 +65,9 @@ public class CarSelectedParameters : MonoBehaviour
             wheelHubs.gameObject.transform.GetChild(1).gameObject.transform.localPosition = new Vector3(0.6f, 0.54f, -2.32f);
             wheelHubs.gameObject.transform.GetChild(2).gameObject.transform.localPosition = new Vector3(-0.6f, 0.54f, 1.52f);
             wheelHubs.gameObject.transform.GetChild(3).gameObject.transform.localPosition = new Vector3(-0.6f, 0.54f, -2.32f);
+
+            collidersCar.gameObject.transform.GetChild(1).gameObject.GetComponent<BoxCollider>().center = new Vector3(0, 3.42f, -0.08f);
+            collidersCar.gameObject.transform.GetChild(1).gameObject.GetComponent<BoxCollider>().size = new Vector3(1.07f, 7.8f, 1.43f);
         }
         if (carSelected == 5)
         {
@@ -58,6 +75,9 @@ public class CarSelectedParameters : MonoBehaviour
             wheelHubs.gameObject.transform.GetChild(1).gameObject.transform.localPosition = new Vector3(0.8f, 0.54f, -2f);
             wheelHubs.gameObject.transform.GetChild(2).gameObject.transform.localPosition = new Vector3(-0.8f, 0.54f, 1.23f);
             wheelHubs.gameObject.transform.GetChild(3).gameObject.transform.localPosition = new Vector3(-0.8f, 0.54f, -2f);
+
+            collidersCar.gameObject.transform.GetChild(1).gameObject.GetComponent<BoxCollider>().center = new Vector3(0, 3.18f, -0.11f);
+            collidersCar.gameObject.transform.GetChild(1).gameObject.GetComponent<BoxCollider>().size = new Vector3(1.23f, 7.3f, 1.49f);
         }
     }
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
 using UnityStandardAssets.Vehicles.Car;
 
@@ -11,12 +10,17 @@ public class UIController : MonoBehaviour
     [SerializeField]
     private GameObject player;
 
-    private float currentTime;
+    [SerializeField]
+    private GameObject pausePanel;
+
+    private bool pauseActivated;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        pausePanel.SetActive(false);
+
+        Time.timeScale = 1.0f;
     }
 
     // Update is called once per frame
@@ -25,6 +29,27 @@ public class UIController : MonoBehaviour
         if (player.GetComponent<CarController>().enabled)
         {
             speedSlider.value = player.GetComponent<CarController>().CurrentSpeed;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseMenu();
+        }
+    }
+
+    public void PauseMenu()
+    {
+        if (!pauseActivated)
+        {
+            pauseActivated = true;
+            pausePanel.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            pauseActivated = false;
+            pausePanel.SetActive(false);
+            Time.timeScale = 1.0f;
         }
     }
 }
